ac3a202ecf13f6b99e98fdf0fcc9f3a64edacf11
KathManaloto <kmanaloto.github@gmail.com>
2025-09-10 17:21:46 +0800

    Done TwoSum

--- src/algorithmic/TwoSum.java
+++ src/algorithmic/TwoSum.java
@@ -14,9 +14,9 @@ public class TwoSum {
     }
 
     public static void main(String[] args) {
-        int[] nums = {2,7,11,15};
+        int[] numbers = {2,7,11,15};
         int target = 9;
-        int[] result = twoSum(nums, target);
+        int[] result = twoSum(numbers, target);
 
         if(result == null) System.out.println("No solution found!");
         else System.out.println("Indices: [" + result[0]  + ", " + result[1] + "]");
