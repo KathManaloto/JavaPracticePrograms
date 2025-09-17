f29f157fd70540786e31b20d43b5953e7c448c9c
KathManaloto <kmanaloto.github@gmail.com>
2025-09-17 16:26:58 +0800

    Done Challenge4. DataTypes - Java Strings (Anagram)

--- src/dataTypes/strings/Challenge4.java
+++ src/dataTypes/strings/Challenge4.java
@@ -41,9 +41,11 @@ public class Challenge4 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+
         String a = scan.next();
         String b = scan.next();
         scan.close();
+
         boolean ret = isAnagram(a, b);
         System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
     }
