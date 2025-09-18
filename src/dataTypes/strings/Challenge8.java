0db889ef852114e89fc702986f957d96fb3d664d
KathManaloto <kmanaloto.github@gmail.com>
2025-09-18 15:01:32 +0800

    Done Challenge8. DataTypes - Java Strings (String Regex)

--- src/dataTypes/strings/Challenge8.java
+++ src/dataTypes/strings/Challenge8.java
@@ -28,13 +28,10 @@ public class Challenge8 {
             String IP = in.next();
             System.out.println(IP.matches(new MyRegex().pattern));
         }
-
     }
 }
 
 class MyRegex{
     String pattern = "^((25[0-5]|2[0-4]\\d|1\\d{2}|0?\\d{1,2})(\\.)){3}"
             + "(25[0-5]|2[0-4]\\d|1\\d{2}|0?\\d{1,2})$";
-
-
 }
